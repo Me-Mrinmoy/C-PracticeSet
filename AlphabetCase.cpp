d1c2cf2fe005c3112b7d0878a4e495f227f9d22a
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-29 17:01:53 +0530

    Add files via upload
     Check Alphabet Case
    Write a program to determine whether an input character is uppercase or lowercase.

---
+++ AlphabetCase.cpp
@@ -0,0 +1,28 @@
+//Check Alphabet Case
+//Write a program to determine whether an input character is uppercase or lowercase.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // take a character --
+    char ch;
+
+    // input from user --
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    // conditions --
+    if (ch >= 'A' && ch <= 'Z') {
+        cout << "This character is uppercase letter" << ch << endl;
+    }
+
+    else if (ch >= 'a' && ch <= 'z') {
+        cout << "This  is a lowercase letter" << ch << endl;
+    }
+
+    else {
+        cout << "Please enter a valid character";
+    }
+}
