1b35cd5d8421638d02c035ff1f628c8d8f07b380
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-29 16:22:27 +0530

    Add files via upload
    Find the Greater Number
    Write a program to find the greater of two numbers.

---
+++ Greater Number.cpp
@@ -0,0 +1,31 @@
+//Find the Greater Number
+//Write a program to find the greater of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int number1;
+    int number2;
+
+    // input from users --
+    cout << "Enter the number1: ";
+    cin >> number1;
+
+    cout << "Enter the number2: ";
+    cin >> number2;
+    
+    // conditions --
+    if (number1 > number2) {
+        cout << "This is a greater number" << number1 << endl;
+    }
+
+    else if (number1 < number2) {
+        cout << "This is a greater number" << number2 << endl;
+    }
+
+    else {
+        cout << "Please enter a valid number!";
+    }
+}
