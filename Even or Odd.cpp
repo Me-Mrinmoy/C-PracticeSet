73b4a5ff8fcdbc458d17368adf093904f5760a71
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-29 16:03:48 +0530

    Add files via upload

---
+++ Even or Odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << "This number is even" << number << endl;
+    }
+
+    else {
+        cout << "This number is odd" << number << endl;
+    }
+    
+}
