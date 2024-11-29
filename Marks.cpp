2080d1bffed08745915cbacede5ef0a263dae4d6
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-29 22:04:15 +0530

    Add files via upload
    Write a program to calculate the grade of a student based on the following criteria:
    
    Marks >= 90: Grade A
    Marks >= 80 and < 90: Grade B
    Marks >= 70 and < 80: Grade C
    Marks >= 60 and < 70: Grade D
    Marks < 60: Fail

---
+++ Marks.cpp
@@ -0,0 +1,41 @@
+//Write a program to calculate the grade of a student based on the following criteria:
+
+//Marks >= 80 and < 90: Grade B
+//Marks >= 70 and < 80: Grade C
+//Marks >= 60 and < 70: Grade D
+//Marks < 60: Fail
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int marks;
+
+    cout << "Enter the marks: ";
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "A";
+    }
+
+    else if (marks >= 80 && marks <= 90) {
+        cout << "B";
+    }
+
+    else if (marks >= 70 && marks <= 80) {
+        cout << "C";
+    }
+
+    else if (marks >= 60 && marks <= 70) {
+        cout << "D";
+    }
+
+    else if (marks >= 50 && marks <= 60) {
+        cout << "E";
+    }
+
+    else {
+        cout << "You are fail go and revise";
+    }
+}
