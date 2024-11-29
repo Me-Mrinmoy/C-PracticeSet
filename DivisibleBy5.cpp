fa3c215af08fb24e62793bb17c84223010c53fa2
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-29 16:33:55 +0530

    Add files via upload
     Check Divisibility
    Write a program to check whether a number is divisible by 5.

---
+++ DivisibleBy5.cpp
@@ -0,0 +1,25 @@
+//Check Divisibility
+//Write a program to check whether a number is divisible by 5.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // take a integar which can be number --
+    int number;
+
+    // input from user --
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // conditions -- 
+    if (number % 5 == 0) {
+        cout << "This number can be divisible" << number << endl;
+    }
+
+    else {
+        cout << "This number can not be divisible" << number << endl;
+    }
+
+}
