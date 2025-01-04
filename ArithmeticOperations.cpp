feed41f9eb23d3901d2aa792312ce8c485800ef8
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-04 17:59:30 +0530

    Add files via upload

---
+++ ArithmeticOperations.cpp
@@ -0,0 +1,48 @@
+// Arithmetic Operations:
+// Write a programe that takes two numbers and an operator (+, -, *, /) as input. Use a switch statement to perform the specified operation.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // take the data type
+    int num1, num2;
+    char op;
+
+    // input the numbers from the user
+    cout << "Enter the num1: ";
+    cin >> num1;
+
+    cout << "Enter the num2: ";
+    cin >> num2;
+
+    // input the operator from the user
+    cout << "Enter the operator: ";
+    cin >> op;
+
+    // condition
+    switch (op)
+    {
+    case '+':
+        cout << "Result is: " << num1 + num2 << endl;
+        break;
+
+    case '-':
+        cout << "Result is: " << num1 - num2 << endl;
+        break;
+
+    case '*':
+        cout << "Result is: " << num1 * num2 << endl;
+        break;
+
+    case '/':
+        cout << "Result is: " << (float)num1 / num2 << endl;
+        break;
+
+    default:
+        cout << "Error! Please enter the correct operator.";
+        break;
+    }
+}
