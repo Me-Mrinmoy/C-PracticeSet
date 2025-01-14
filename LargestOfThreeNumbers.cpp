958e06dcee86759ffb7134f7adb4cc28b7cbd1e1
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-14 18:56:37 +0530

    Add files via upload

---
+++ LargestOfThreeNumbers.cpp
@@ -0,0 +1,44 @@
+// Find the Largest of Three Numbers: 
+// Write a function that takes three integers and returns the largest one.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int num1;
+    int num2;
+    int num3;
+
+    cout << "Enter the num1: ";
+    cin >> num1;
+
+    cout << "Enter the num2: ";
+    cin >> num2;
+
+    cout << "Enter the num3: ";
+    cin >> num3;
+
+    if (num1 > num2) {
+        cout << "Largest is: " << num1 << endl;
+    }
+
+    else if (num2 > num3) 
+    {
+        cout << "Largest is: " << num2 << endl;
+    }
+
+    else if (num3 > num2)
+    {
+        cout << "Largest is: " << num3 << endl;
+    }
+    
+
+    else
+    {
+        cout << "There is no largest number please enter the number correctly" << endl;
+    }
+    
+    
+    
+}
