31a79c68e575eceda92f4c4129d4a7162ae71b9b
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-02-02 12:57:17 +0530

    Update comments and formatting in PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,5 +1,8 @@
 // Check Prime Number: 
 // Write a function that checks if a given number is prime.
+// What is prime number??
+// A whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1.
+// Example - (2, 3, 5, 7, 11).
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -25,4 +28,5 @@ int main () {
 
     return 0;
 }
+
 
