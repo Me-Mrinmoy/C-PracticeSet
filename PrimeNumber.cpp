22ba1fa6a37c1358f445ce904e141a0793b540b8
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-02-02 12:47:48 +0530

    Refactor PrimeNumber.cpp with improved comments

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,21 +1,28 @@
 // Check Prime Number: 
 // Write a function that checks if a given number is prime.
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main () {
 
+    // takes an integar num---
     int num;
 
+    // input from user---
     cout << "Enter the num: ";
     cin >> num;
 
-    if (num % 2 == 0) {
+    // conditions---
+    if (num % 2 == 0) 
+    {
         cout << "This number is prime";
     }
-
-    else {
+    else 
+    {
         cout << "This number is not prime";
     }
+
+    return 0;
 }
+
