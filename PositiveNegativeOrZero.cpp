bb6d2b2af746d496db947e3d145c2e4481188e58
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-02-02 13:05:36 +0530

    Refactor PositiveNegativeOrZero.cpp for clarity
    Refactored code to improve readability and structure. Added comments for clarity.

--- PositiveNegativeOrZero.cpp
+++ PositiveNegativeOrZero.cpp
@@ -1,26 +1,37 @@
+// Check if a given number is positive, negative or zero.
+// Input = -5.
+// Output = Negative.
+
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    int number;
+    // takes an integar num---
+    int num;
 
+    // takes an input from the user---
     cout << "Enter a number: ";
-    cin >> number;
+    cin >> num;
 
-    if (number > 0) {
-        cout << "This number is positive" << number << endl;
+    // conditions---
+    if (num > 0)
+    {
+        cout << num << " is a positive number" << endl;
     }
-
-    if (number < 0) {
-        cout << "This number is negative" << number << endl;
+    else if (num < 0) 
+    {
+        cout << num << " is a negative number" << endl;
     }
-
-    else if (number == 0) {
-        cout << "This is zero" << number << endl;
+    else if (num == 0) 
+    {
+        cout << num << " is zero" << endl;
     }
-
-    else {
+    else 
+    {
         cout << "Please enter a valid number!";
     }
+
+    return 0;
 }
+
