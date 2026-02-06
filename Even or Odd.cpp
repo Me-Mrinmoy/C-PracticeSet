0389688de26571945b8f1ff8caa82af9e343512f
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-02-06 11:31:53 +0530

    Refactor Even or Odd program with improved formatting

--- Even or Odd.cpp
+++ Even or Odd.cpp
@@ -14,15 +14,19 @@ int main () {
     cout << "Enter a number: ";
     cin >> number;
 
-    if (number % 2 == 0) {
+    // conditions---
+    if (number % 2 == 0) 
+    {
         cout << "This number is even" << number << endl;
     }
-
-    else {
+    else 
+    {
         cout << "This number is odd" << number << endl;
     }
-    
+
+    return 0;
 
 }
+
 
 
