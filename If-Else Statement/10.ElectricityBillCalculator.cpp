725d3eae3d2097353c159e65b0695b47b59c1fd5
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-02-09 20:39:15 +0530

    Add files via upload

---
+++ If-Else Statement/10.ElectricityBillCalculator.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int unit;
+    cout << "Enter the electricity unit: "; 
+    cin >> unit;
+
+    if (unit == 100)
+    {
+        cout << "First Unit Free";
+    }
+    else if (unit >= 101 && unit <= 200)
+    {
+        cout << "Total cost is: " << 5 * unit << endl;
+    }
+    else if (unit >= 201 && unit <= 300)
+    {
+        cout << "Total cost is: " << 10 * unit << endl;
+    }
+    else if (unit > 300)
+    {
+        cout << "Total cost is: " << 12 * unit << endl;
+    }
+    else 
+    {
+        cout << "Invalid input" << endl;
+    }
+
+    return 0;
+}
