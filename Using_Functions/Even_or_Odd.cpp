7596ed5646cae3d9f84fca61b4e73f54d1a1b59b
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-03-02 20:38:31 +0530

    Add Even or Odd checker in Even_or_Odd.cpp

---
+++ Using_Functions/Even_or_Odd.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void checkN(int n)
+{
+    if (n % 2 == 0)
+    {
+        cout << n << " is even.";
+    }
+    else 
+    {
+        cout << n << " is odd.";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "enter the n: ";
+    cin >> n;
+
+    checkN(n);
+
+    return 0;
+}
