bda61994a557dc35cb7c2d7accda469045fa9b21
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-03-06 10:55:18 +0530

    Add addition function to perform checks with user input

---
+++ Using_Functions/addition.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void checkAdd(int n)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n << " + " << i << " = " << n + i << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "enter the n: ";
+    cin >> n;
+
+    checkAdd(n);
+
+    return 0;
+}
