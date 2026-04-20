73d1383e509cb9261ea9204c9f68495456ebd436
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2026-04-20 12:19:05 +0530

    Update 10.ElectricityBillCalculator.cpp

--- If-Else Statement/10.ElectricityBillCalculator.cpp
+++ If-Else Statement/10.ElectricityBillCalculator.cpp
@@ -18,7 +18,7 @@ int main()
     {
         cout << "First Unit Free";
     }
-    // when the electricity unit between  
+    // when the electricity unit between 101 
     else if (unit >= 101 && unit <= 200)  
     {
         cout << "Total cost is: " << 5 * unit << endl;
